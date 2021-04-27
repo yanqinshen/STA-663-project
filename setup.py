fbd7b1e6bdf54c171ec38e1aad6f84630b5a91ce
yanqinshen <46582115+yanqinshen@users.noreply.github.com>
2021-04-27 11:40:52 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='SSVD',
+      version='0.1',
+      description='The sparse SVD algorithm',
+      author='Nancy Fu, Gojia Xu, Yanqin Shen',
+      author_email='ys309@duke.edu',
+      packages=['SSVD'],
+      zip_safe=False)
